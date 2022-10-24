1186458add2ab79dd3228a09390445cec861c9ad
alvaro <alvarorajic2@gmail.com>
2022-10-24 17:03:28 +0200

    fixed update user controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -91,7 +91,15 @@ const updateUser = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const user = await User.findById(id);
+    const user = await User.findByIdAndUpdate(
+      id,
+      {
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      },
+      { new: true }
+    );
     res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
